e8548b0b6326511f326df48e0299b58291a2e41b
Joyce <lapinette2002@ymail.com>
2024-10-02 15:57:35 +0200

    fix mutli token request

--- app/server.js
+++ app/server.js
@@ -11,6 +11,16 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
+// public assets
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+app.use('/coverage', express.static(path.join(__dirname, '..', 'coverage')));
+
+// ejs for view templates
+app.engine('.html', ejs.__express);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
 //middleware token bucket
 app.use(async (req, res, next) => {
     let requestDate = Date.now();
@@ -57,16 +67,6 @@ app.use(async (req, res, next) => {
 
     next();
 });
-
-// public assets
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
-app.use('/coverage', express.static(path.join(__dirname, '..', 'coverage')));
-
-// ejs for view templates
-app.engine('.html', ejs.__express);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
 
 // load route
 import * as routes from './route.js';
